e4d1350522c1c62bdd5299a865be4ead63e424c0
Adam Zahradník <adam@zahradnik.xyz>
2023-11-13 12:50:52 +0100

    web: add bot detail

--- web/proboj/bots/urls.py
+++ web/proboj/bots/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from proboj.bots.views import BotListView
+from proboj.bots.views import BotDetailView, BotListView
 
 urlpatterns = [
     path("games/<int:game>/bots/", BotListView.as_view(), name="bot_list"),
+    path("games/<int:game>/bots/<int:pk>/", BotDetailView.as_view(), name="bot_detail"),
 ]
--- web/proboj/bots/views.py
+++ web/proboj/bots/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from proboj.bots.models import Bot
 from proboj.games.mixins import GameMixin
@@ -12,3 +12,12 @@ class BotQuerySetMixin:
 
 class BotListView(LoginRequiredMixin, BotQuerySetMixin, GameMixin, ListView):
     template_name = "bots/list.html"
+
+
+class BotDetailView(LoginRequiredMixin, BotQuerySetMixin, GameMixin, DetailView):
+    template_name = "bots/detail.html"
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["versions"] = self.object.botversion_set.all()
+        return ctx
