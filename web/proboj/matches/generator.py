2deec8ff724f30905b84fd76351c5b6bd7b206d7
Adam Zahradník <adam@zahradnik.xyz>
2023-11-14 19:17:33 +0100

    web: match generator

---
+++ web/proboj/matches/generator.py
@@ -0,0 +1,49 @@
+import random
+from dataclasses import dataclass
+
+from proboj.bots.models import Bot, BotVersion
+from proboj.games.models import Configuration, Game
+from proboj.matches.models import Match, MatchBot
+
+
+@dataclass
+class MatchPlan:
+    game: Game
+    configuration: Configuration
+    bots: list[BotVersion]
+
+    def save(self) -> Match:
+        match = Match.objects.create(game=self.game, configuration=self.configuration)
+        for bot in self.bots:
+            MatchBot.objects.create(match=match, bot_version=bot)
+
+        return match
+
+    def to_json(self) -> dict:
+        return {
+            "players": [f"{bv.bot.name}_v{bv.number}" for bv in self.bots],
+            "args": self.configuration.args,
+        }
+
+
+def generate_matches(game: Game, number: int = 1) -> list[MatchPlan]:
+    configurations = Configuration.objects.filter(game=game, is_enabled=True).all()
+    bots = Bot.objects.filter(game=game, is_enabled=True)
+    latest_versions = list(
+        BotVersion.objects.filter(bot__in=bots, is_enabled=True)
+        .exclude(compiled="")
+        .select_related("bot")
+        .order_by("bot_id", "-number")
+        .distinct("bot_id")
+    )
+
+    matches = []
+    for i in range(number):
+        config: Configuration = random.choice(configurations)
+        bot_count = len(latest_versions)
+        if config.max_bots and bot_count > config.max_bots:
+            bot_count = config.max_bots
+        bots: list[BotVersion] = random.sample(latest_versions, k=bot_count)
+        matches.append(MatchPlan(game, config, bots))
+
+    return matches
